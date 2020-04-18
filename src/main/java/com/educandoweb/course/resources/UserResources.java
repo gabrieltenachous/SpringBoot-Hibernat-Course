0ba38b2990de78e0592334c3ac2cd614e59e5fdf
Gabriel Oliveira <gabriel_oliveira_f_sousa@outlook.com>
2020-04-18 19:03:17 -0300

    First Commit

---
+++ src/main/java/com/educandoweb/course/resources/UserResources.java
@@ -0,0 +1,19 @@
+package com.educandoweb.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.educandoweb.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/user")
+public class UserResources {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L,"Maria","maria@gmail","9999","123546");
+		return ResponseEntity.ok().body(u);
+	}
+}
