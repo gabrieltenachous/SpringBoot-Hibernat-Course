a8c0d22260b29ad77e716ebed2ccd94a42768525
Gabriel Oliveira <gabriel_oliveira_f_sousa@outlook.com>
2020-04-18 22:09:28 -0300

    Order,Instant ISO 8601 part2

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -11,11 +11,11 @@ import com.educandoweb.course.repositories.UserRepository;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repository;
 	
-	public List<User> findAll(){
+	public List<User> findAll() {
 		return repository.findAll();
 	}
 	
