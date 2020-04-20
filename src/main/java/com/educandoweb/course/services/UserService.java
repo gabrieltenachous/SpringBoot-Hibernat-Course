22ca75353b45b944510ff10f6624b7eb0968ef4b
Gabriel Oliveira <gabriel_oliveira_f_sousa@outlook.com>
2020-04-20 11:39:42 -0300

    User update

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -24,6 +24,20 @@ public class UserService {
 		return obj.get();
 	}
 	
+	public User update(Long id,User obj) {
+		User entity = repository.getOne(id);
+		
+		updateData(entity,obj);
+		return repository.save(entity);
+	}
+	
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+		
+	}
+	 
 	public void delete(Long id) {
 		repository.deleteById(id);
 	}
